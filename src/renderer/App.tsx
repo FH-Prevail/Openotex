ebe97234715bf741c61743d2a252c14bc4fdd476
sina <p42872@fhooe.at>
2025-11-03 09:03:05 +0100

    Updater issue fixed

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1269,7 +1269,7 @@ Your conclusions go here.
                     // Delay the check by 3 seconds to let the app fully load
                     setTimeout(async () => {
                         try {
-                            const CURRENT_VERSION = '1.0.0';
+                            const CURRENT_VERSION = '1.0.1';
                             const VERSION_CHECK_URL = 'https://openotex.com/downloads/Openotex-Setup-';
                             const DOWNLOAD_PAGE_URL = 'https://openotex.com/#download';
 
